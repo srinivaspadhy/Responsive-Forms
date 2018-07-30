15be9c4f89c8c62f017959b1844a830364fed6a7
srinivas <srinivaspadhybm@gmail.com>
2018-07-30 06:25:43 -0700

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Route } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login';
+  password = "pass";
+  emailId = "email";
+  onlogin()
+  {
+    localStorage.setItem('key', this.emailId);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CredentialsComponent } from './credentials/credentials.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+const loginroute: Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  }, {
+    path: "final",
+    component: CredentialsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CredentialsComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(loginroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
